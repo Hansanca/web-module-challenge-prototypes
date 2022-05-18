f372252f256779ff0703a29130b127801ca3ce12
Hansanca <hansanca@gmail.com>
2022-05-18 15:40:09 -0400

    done

--- index.js
+++ index.js
@@ -15,8 +15,20 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(food) {
+  this.stomach.push(food);
+};
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 
@@ -36,8 +48,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0;
+  this.odometer = 0;
+  
+}
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons 
 }
 
 
@@ -49,8 +68,20 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
 
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+}
+Baby.prototype.eat = function() {
+  Person.prototype.eat()
+}
+Baby.prototype.poop = function() {
+  Person.prototype.poop()
 }
 
 
